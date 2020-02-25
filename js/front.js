cd886c88ca07eef3042f8f1c38e782d515ab480b
virtual <jeanine.mt@gmail.com>
2020-02-25 16:39:01 -0700

    change manifest so it is root relative

--- js/front.js
+++ js/front.js
@@ -378,12 +378,12 @@ if('serviceWorker' in navigator) {
   navigator.serviceWorker
       .register('/sw.js', { scope: '/' })
       .then(function(registration) {
-          console.log('Service Worker Registered');
+          //console.log('Service Worker Registered');
       });
 
   navigator.serviceWorker
       .ready
       .then(function(registration) {
-          console.log('Service Worker Ready');
+          //console.log('Service Worker Ready');
       });
 }
